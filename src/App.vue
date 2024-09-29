22a19a030666af690e92612953a303aeca6016e5
翟金宇 <3422801092@qq.com>
2024-09-29 12:01:34 +0800

    提交

--- src/App.vue
+++ src/App.vue
@@ -17,7 +17,7 @@ import HelloWorld from './components/HelloWorld.vue'
       <HelloWorld msg="You did it!" />
 
       <nav>
-        <RouterLink to="/">Home</RouterLink>
+        <RouterLink to="/">主页</RouterLink>
         <RouterLink to="/about">About</RouterLink>
       </nav>
     </div>
